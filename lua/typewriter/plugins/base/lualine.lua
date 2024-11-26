dd05d478f58441b184a00954a5dd21fb3bdccb2c
Cristian Dinu <cristian.virgil.dinu@gmail.com>
2024-11-27 00:42:09 +0200

    refactor(lualine): Use `opts` for configuration

--- lua/typewriter/plugins/base/lualine.lua
+++ lua/typewriter/plugins/base/lualine.lua
@@ -1,26 +1,23 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-        local theme = require("lualine.themes.gruvbox")
-        local lazy_status = require("lazy.status")
-        require("lualine").setup({
-            options = {
-                theme = theme,
-            },
-            sections = {
-                lualine_x = {
-                    -- Lazy pending updates count
-                    {
-                        lazy_status.updates,
-                        cond = lazy_status.has_updates,
-                        color = { fg = "#ff9e64" },
-                    },
-                    { "encoding" },
-                    { "fileformat" },
-                    { "filetype" },
-                },
-            },
-        })
-    end,
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = function()
+		local opts = {
+			options = require("lualine.themes.gruvbox"),
+			sections = {
+				lualine_x = {
+					-- Lazy pending updates count
+					{
+						require("lazy.status").updates,
+						cond = require("lazy.status").has_updates,
+						color = { fg = "#ff9e64" },
+					},
+					{ "encoding" },
+					{ "fileformat" },
+					{ "filetype" },
+				},
+			},
+		}
+		return opts
+	end,
 }
