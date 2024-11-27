fee6ffc5fdcc17c6e40c052fc833f793080c72f6
Cristian Dinu <cristian.virgil.dinu@gmail.com>
2024-11-27 19:28:29 +0200

    refactor(treesitter): Use `opts` for configuration

--- lua/typewriter/plugins/editor/treesitter.lua
+++ lua/typewriter/plugins/editor/treesitter.lua
@@ -3,52 +3,41 @@ return {
 	event = { "BufReadPre", "BufNewFile" },
 	build = ":TSUpdate",
 	dependencies = {},
-	config = function()
-		local treesitter = require("nvim-treesitter.configs")
-
-		-- Configure treesitter
-		treesitter.setup({
-			-- Enable syntax highlighting
-			highlight = {
-				enable = true,
+	opts = {
+		-- Enable syntax highlighting
+		highlight = {
+			enable = true,
+		},
+		-- Enable indentation
+		indent = {
+			enable = true,
+		},
+		-- Ensure these language parsers are installed
+		ensure_installed = {
+			-- Programming languages
+			"c_sharp",
+			"lua",
+			-- Markup and styling
+			"markdown",
+			"markdown_inline",
+			-- Data formats
+			"json",
+			"yaml",
+			-- Shell and scripting
+			"bash",
+			"fish",
+			-- Others
+			"dockerfile",
+			"gitignore",
+		},
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "<C-space>",
+				node_incremental = "<C-space>",
+				scope_incremental = false,
+				node_decremental = "<bs>",
 			},
-
-			-- Enable indentation
-			indent = { enable = true },
-
-			-- Ensure these language parsers are installed
-			ensure_installed = {
-				-- Programming languages
-				"c_sharp",
-                "lua",
-
-				-- Markup and styling
-				"markdown",
-				"markdown_inline",
-
-				-- Data formats
-				"json",
-				"yaml",
-
-				-- Shell and scripting
-				"bash",
-				"fish",
-
-				-- Others
-				"dockerfile",
-				"gitignore",
-			},
-
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<C-space>",
-					node_incremental = "<C-space>",
-					scope_incremental = false,
-					node_decremental = "<bs>",
-				},
-			},
-		})
-	end,
+		},
+	},
 }
-
