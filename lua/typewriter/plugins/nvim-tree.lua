b2a6ab541a4da33caf4d29c64ce26383b143f622
Cristian Dinu <cristian.virgil.dinu@gmail.com>
2025-02-27 14:32:54 +0200

    feat(nvim-tree): Open file tree in floating window
    When starting Neovim with `nvim dir/`, then `nvim-tree` will occupy the
    entire screen. When you open a file with `nvim file`, the file will show
    up. Now when activating `nvim-tree` with `:NvimTreeToggle` it will be
    shown in a floating Neovim window that is centered and occupying 80% of
    the editor window.

--- lua/typewriter/plugins/nvim-tree.lua
+++ lua/typewriter/plugins/nvim-tree.lua
@@ -1,9 +1,39 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 return {
 	"nvim-tree/nvim-tree.lua",
 	dependencies = {
 		"nvim-tree/nvim-web-devicons",
+	},
+	opts = {
+		disable_netrw = true,
+		hijack_netrw = true,
+		hijack_cursor = true,
+		hijack_unnamed_buffer_when_opening = false,
+		view = {
+			float = {
+				enable = true,
+				open_win_config = function()
+					local screen_w = vim.opt.columns:get()
+					local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+					local w_h = math.floor(0.9 * screen_w)
+					local s_h = math.floor(0.9 * screen_h)
+					local center_x = (screen_w - w_h) / 2
+					local center_y = ((vim.opt.lines:get() - s_h) / 5) - vim.opt.cmdheight:get()
+					return {
+						border = "rounded",
+						relative = "editor",
+						row = center_y,
+						col = center_x,
+						width = w_h,
+						height = s_h,
+					}
+				end,
+			},
+			width = function()
+				return math.floor(vim.opt.columns:get() * 5)
+			end,
+		},
+		filesystem_watchers = {
+			enable = true,
+		},
 	},
 }
