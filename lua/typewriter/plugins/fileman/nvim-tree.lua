88fafbe83992716588520be741ec67de84afc2db
Cristian Dinu <cristian.virgil.dinu@gmail.com>
2025-04-11 00:05:19 +0300

    refactor(fileman): Use table instead of function for `keys`
    When setting up `nvim-tree` file manager there is no need to use a
    function to create the keybindings. The `nvim_tree` object was never
    used.

--- lua/typewriter/plugins/fileman/nvim-tree.lua
+++ lua/typewriter/plugins/fileman/nvim-tree.lua
@@ -3,13 +3,9 @@ return {
 	dependencies = {
 		"nvim-tree/nvim-web-devicons",
 	},
-	keys = function()
-		local nvim_tree = require("nvim-tree")
-
-		return {
-			{ "<LEADER>e", ":NvimTreeFindFileToggle<CR>", desc = "Toggle file tree" },
-		}
-	end,
+	keys = {
+		{ "<LEADER>e", ":NvimTreeFindFileToggle<CR>", desc = "Toggle file tree" },
+	},
 	opts = {
 		disable_netrw = true,
 		hijack_netrw = true,
